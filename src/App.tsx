2ab95e8a412806eb0260427776aae00f65f8ae6c
Aaron Yattan <102608465+Aaron0423@users.noreply.github.com>
2023-11-20 12:58:00 +0800

    merge stuff so SDA

--- src/App.tsx
+++ src/App.tsx
@@ -20,18 +20,6 @@ import AboutMe from "./components/RegisterPagePD/AboutMe";
 import UploadPhoto from "./components/RegisterPagePD/UploadPhoto";
 import UploadVideo from "./components/RegisterPagePD/UploadVideo";
 
-const initRegPage = () => {
-  const [regPage, setRegPage] = useState(0);
-
-  return (
-    <div>
-      <FormPageContext.Provider value={{ regPage, setRegPage }}>
-        <RegisterForm />
-      </FormPageContext.Provider>
-    </div>
-  );
-};
-
 function App() {
   return (
     <Router>
@@ -41,12 +29,12 @@ function App() {
         <Route path="/home" element={<Dashboard />} />
         <Route path="/password_questions" element={<PasswordQuestionPage />} />
 
-        <Route path="/register_page" element={initRegPage()} />
-        <Route path="/forget_password_qna" element={<ForgotPasswordQNA/>} />
-        <Route path="/additional_info" element={<AdditionalInfo/>} />
-        <Route path="/about_me" element={<AboutMe/>} />
-        <Route path="/upload_photo" element={<UploadPhoto/>} />
-        <Route path="/upload_video" element={<UploadVideo/>} />
+        <Route path="/register_page" element={<RegisterForm></RegisterForm>} />
+        <Route path="/forget_password_qna" element={<ForgotPasswordQNA />} />
+        <Route path="/additional_info" element={<AdditionalInfo />} />
+        <Route path="/about_me" element={<AboutMe />} />
+        <Route path="/upload_photo" element={<UploadPhoto />} />
+        <Route path="/upload_video" element={<UploadVideo />} />
 
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
