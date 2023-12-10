143d5bc78938ad050d51aca58ec2736a6e7f017f
YouzaW <saleh08@graduate.utm.my>
2023-12-10 22:02:42 +0800

    linking calendar to database

--- src/components/CalendarPage/CalendarMain.tsx
+++ src/components/CalendarPage/CalendarMain.tsx
@@ -1,139 +1,79 @@
-import React, { useState } from "react";
-import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
-import "./CalendarMain.css";
+import React, { useEffect, useState } from "react";
+import dayjs, { Dayjs } from "dayjs";
+import type { BadgeProps, CalendarProps } from "antd";
+import { Badge, Calendar } from "antd";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "../../firebase";
 
-interface Event {
-  id: number;
-  date: Date;
-  title: string;
+interface EventData {
+  eventName: string;
+  type: BadgeProps["status"];
 }
 
-const CalendarMain: React.FC = () => {
-  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
-  const [eventName, setEventName] = useState<string>("");
-  const [events, setEvents] = useState<Event[]>([]);
+const fetchEventsFromFirestore = async (
+  month: Dayjs,
+  setData: React.Dispatch<React.SetStateAction<Record<string, EventData[]>>>
+) => {
+  try {
+    const startOfMonth = month.startOf("month").format("YYYY-MM-DD");
+    const endOfMonth = month.endOf("month").format("YYYY-MM-DD");
+    const eventsRef = collection(db, "Event");
+    const q = query(
+      eventsRef,
+      where("eventDate", ">=", startOfMonth),
+      where("eventDate", "<=", endOfMonth)
+    );
+    const querySnapshot = await getDocs(q);
 
-  const Date_Click_Fun = (date: Date) => {
-    setSelectedDate(date);
-  };
+    const eventData: Record<string, EventData[]> = {};
 
-  const Event_Data_Update = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEventName(event.target.value);
-  };
+    querySnapshot.forEach((doc) => {
+      const data = doc.data();
+      const eventDate = dayjs(data.eventDate);
+      const dateStr = eventDate.format("YYYY-MM-DD");
 
-  const Create_Event_Fun = () => {
-    if (selectedDate && eventName) {
-      const newEvent: Event = {
-        id: new Date().getTime(),
-        date: selectedDate,
-        title: eventName,
-      };
-      setEvents([...events, newEvent]);
-      setSelectedDate(null);
-      setEventName("");
-      setSelectedDate(newEvent.date);
-    }
-  };
-
-  const Update_Event_Fun = (eventId: number, newName: string) => {
-    const updatedEvents = events.map((event) => {
-      if (event.id === eventId) {
-        return {
-          ...event,
-          title: newName,
-        };
+      if (!eventData[dateStr]) {
+        eventData[dateStr] = [];
       }
-      return event;
+
+      eventData[dateStr].push({
+        eventName: data.eventName,
+        type: "success", // Set the desired status for all badges
+      });
     });
-    setEvents(updatedEvents);
-  };
 
-  const Delete_Event_Fun = (eventId: number) => {
-    const updatedEvents = events.filter((event) => event.id !== eventId);
-    setEvents(updatedEvents);
-  };
+    setData(eventData);
+  } catch (error) {
+    console.error("Error fetching event data:", error);
+  }
+};
 
-  return (
-    <div className="CalendarMain">
-      <h1>Manage Events Calendar</h1>
-      <div className="container">
-        <div className="calendar-container">
-          <Calendar
-            value={selectedDate}
-            onClickDay={Date_Click_Fun}
-            tileClassName={({ date }: { date: Date }) =>
-              selectedDate &&
-              date.toDateString() === selectedDate.toDateString()
-                ? "selected"
-                : events.some(
-                    (event) => event.date.toDateString() === date.toDateString()
-                  )
-                ? "event-marked"
-                : ""
-            }
-          />
-        </div>
-        <div className="event-container">
-          {selectedDate && (
-            <div className="event-form">
-              <h2>Create Event</h2>
-              <p>Selected Date: {selectedDate.toDateString()}</p>
-              <input
-                type="text"
-                placeholder="Event Name"
-                value={eventName}
-                onChange={Event_Data_Update}
-              />
-              <button className="create-btn" onClick={Create_Event_Fun}>
-                Click Here to Add Event
-              </button>
-            </div>
-          )}
-          {events.length > 0 && selectedDate && (
-            <div className="event-list">
-              <h2>My Created Event List</h2>
-              <div className="event-cards">
-                {events.map((event) =>
-                  event.date.toDateString() === selectedDate.toDateString() ? (
-                    <div key={event.id} className="event-card">
-                      <div className="event-card-header">
-                        <span className="event-date">
-                          {event.date.toDateString()}
-                        </span>
-                        <div className="event-actions">
-                          <button
-                            className="update-btn"
-                            onClick={() =>
-                              Update_Event_Fun(
-                                event.id,
-                                prompt("ENTER NEW TITLE") || ""
-                              )
-                            }
-                          >
-                            Update Event
-                          </button>
-                          <button
-                            className="delete-btn"
-                            onClick={() => Delete_Event_Fun(event.id)}
-                          >
-                            Delete Event
-                          </button>
-                        </div>
-                      </div>
-                      <div className="event-card-body">
-                        <p className="event-title">{event.title}</p>
-                      </div>
-                    </div>
-                  ) : null
-                )}
-              </div>
-            </div>
-          )}
-        </div>
+const CalendarMain: React.FC = () => {
+  const [events, setEvents] = useState<Record<string, EventData[]>>({});
+
+  useEffect(() => {
+    const currentMonth = dayjs(new Date());
+    fetchEventsFromFirestore(currentMonth, setEvents);
+  }, []);
+
+  const cellRender: CalendarProps<Dayjs>["cellRender"] = (value) => {
+    const dateStr = value.format("YYYY-MM-DD");
+    const listData = events[dateStr] || [];
+
+    return (
+      <div className="events">
+        <ul>
+          {listData.map((item, index) => (
+            <li key={index}>
+              <b>{item.eventName}</b>
+            </li>
+          ))}
+        </ul>
       </div>
-    </div>
-  );
+    );
+  };
+
+  return <Calendar cellRender={cellRender} />;
 };
 
 export default CalendarMain;
